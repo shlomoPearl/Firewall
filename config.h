628de54e1f177ab6338fbf949f29131b7df900d4
shlomoPearl <shlomoper94@gmail.com>
2026-08-16 02:54:41 +0300

    add config vars data

---
+++ config.h
@@ -0,0 +1,6 @@
+#pragma once
+#define RULES_FILE "rules.json"
+#define IP_LST_N "ip_blacklist"
+#define PORT_LST_N "port_blacklist"
+#define MAX_TCP_HEADER_BYTES 60  // Maximum TCP header size in bytes
+#define RULES_FILE_SIZE 1024  // Maximum size of the rules file in bytes
