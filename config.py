ccdbc594f4897be231f8e13878a81a856c0873a5
shlomoPearl <shlomoper94@gmail.com>
2026-08-16 02:57:54 +0300

    remove py files, currentl working only with c

--- config.py
+++
@@ -1,3 +0,0 @@
-RULES_FILE = "rules.json"
-IP_LST_N = "ip_blacklist"
-PORt_LST_N = "port_blacklist"
--- rules_parser.py
+++
@@ -1,8 +0,0 @@
-import json
-from config import *
-
-with open(RULES_FILE, 'r') as f:
-    rules = json.load(f)
-
-ip_blacklist = rules[IP_LST_N]
-port_blacklist = rules[PORt_LST_N]
