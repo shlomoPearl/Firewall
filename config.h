d21e70ecaa80760c25dea6665e69ecf3acf8a285
shlomoPearl <shlomoper94@gmail.com>
2026-08-16 23:19:41 +0300

    add notify buffer size

--- config.h
+++ config.h
@@ -3,4 +3,4 @@
 #define IP_LST_N "ip_blacklist"
 #define PORT_LST_N "port_blacklist"
 #define MAX_TCP_HEADER_BYTES 60  // Maximum TCP header size in bytes
-#define RULES_FILE_SIZE 1024  // Maximum size of the rules file in bytes
+#define BUFFER_NOTIFY_SIZE 4096  // Maximum size of the rules file in bytes
