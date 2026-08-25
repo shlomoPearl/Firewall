076048357f28dad25c40e7fe3ec0851e65658059
shlomoPearl <shlomoper94@gmail.com>
2026-08-25 03:03:14 +0300

    pass file/dir name to intotify

--- rules_notify.c
+++ rules_notify.c
@@ -1,13 +1,13 @@
 #include "rules_notify.h"
 
-int setup_inotify() {
+int setup_inotify(const char* rules_file) {
     int inotify_fd = inotify_init();
     if (inotify_fd < 0) {
         perror("inotify_init");
         return -1;
     }
 
-    int watch_descriptor = inotify_add_watch(inotify_fd, RULES_FILE, IN_CLOSE_WRITE | IN_MOVED_TO);
+    int watch_descriptor = inotify_add_watch(inotify_fd, rules_file, IN_CLOSE_WRITE | IN_MOVED_TO);
     if (watch_descriptor < 0) {
         perror("inotify_add_watch");
         close(inotify_fd);
@@ -24,11 +24,11 @@ int watch_rules_changes(int inotify_fd) {
     while (i < length) {
         event = (struct inotify_event*)&buffer[i];
         if (event->mask & IN_CLOSE_WRITE) {
-            printf("Rules file changed, reloading...\n");
+            printf("Rules file changed - IN_CLOSE_WRITE, reloading...\n");
             return 1; 
         }
         if (event->mask & IN_MOVED_TO) {
-            printf("Rules file moved to, reloading...\n");
+            printf("Rules file moved to - IN_MOVED_TO, reloading...\n");
             return 1; 
         }
         i += sizeof(struct inotify_event) + event->len;
