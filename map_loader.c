bc616ec9281517e52c5da08d52eca504375334ef
shlomoPearl <shlomoper94@gmail.com>
2026-09-02 01:22:37 +0300

    seperate vaild port function

--- map_loader.c
+++ map_loader.c
@@ -14,10 +14,24 @@ int extract_valid_ip(cJSON* ip, uint32_t* out_ips){
     return -1;
 }
 
+int extract_valid_port(cJSON* port, uint16_t* out_port){
+    if (cJSON_IsString(port)) {
+        const char* port_str = port->valuestring;
+        int port_num = atoi(port_str);
+        if (port_num < 1 || port_num > 65535) {
+            fprintf(stderr, "Invalid port number: %s\n", port_str);
+            return -1;
+        }
+        *out_port = (uint16_t)port_num;
+        return 0;
+    }
+    return -1;
+}
+
 int ip_list_2_map(cJSON* ip_list, struct bpf_map *black_map) {
     cJSON* ip = NULL;
     cJSON_ArrayForEach(ip, ip_list) {
-        uint32_t *ip_key;
+        uint32_t *ip_key = NULL;
         if (extract_valid_ip(ip, ip_key) != 0){
             fprintf(stderr, "Failed to extract IP: %s\n", ip->valuestring);
             return -1;    
@@ -33,18 +47,15 @@ int ip_list_2_map(cJSON* ip_list, struct bpf_map *black_map) {
 int port_list_2_map(cJSON* port_list, struct bpf_map *black_map) {
     cJSON* port = NULL;
     cJSON_ArrayForEach(port, port_list) {
-        if (cJSON_IsString(port)) {
-            const char* port_str = port->valuestring;
-            uint16_t port_num = (uint16_t)atoi(port_str);
-            if (port_num < 1 || port_num > 65535) {
-                fprintf(stderr, "Invalid port number: %s\n", port_str);
-                continue;
-            }
-            uint16_t port_key = (uint16_t)port_num; 
-            uint8_t value = 1; 
-            if (bpf_map_update_elem(bpf_map__fd(black_map), &port_key, &value, BPF_ANY) != 0) {
-                fprintf(stderr, "Failed to update black_map for port: %s\n", port_str);
-            }
+        uint16_t* port_key= NULL;
+        if (extract_valid_port(port, port_key) != 0){
+            fprintf(stderr, "Failed to extract PORT: %s\n", port->valuestring);
+            return -1;
+        }
+        uint8_t value = 1; 
+        if (bpf_map_update_elem(bpf_map__fd(black_map), &port_key, &value, BPF_ANY) != 0) {
+            fprintf(stderr, "Failed to update black_map for port: %s\n", port->valuestring);
+            return -1;
         }
     }
     return 0;
--- map_loader.h
+++ map_loader.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <stdlib.h>
 #include <net/if.h>
 #include <arpa/inet.h>
 #include <bpf/libbpf.h>
@@ -6,5 +7,6 @@
 #include "lib/cJSON.h"
 
 int extract_valid_ip(cJSON* ip, uint32_t* out_ips);
+int extract_valid_port(cJSON* port, uint16_t* out_ports);
 int ip_list_2_map(cJSON* ip_list, struct bpf_map *black_map);
 int port_list_2_map(cJSON* port_list, struct bpf_map *black_map);
