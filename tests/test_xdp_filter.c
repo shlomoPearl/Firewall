70a9ed54d9aaae1425089144be8c12188057a6ab
shlomoPearl <shlomoper94@gmail.com>
2026-09-04 00:02:21 +0300

    refactor and better order

--- tests/test_xdp_filter.c
+++ tests/test_xdp_filter.c
@@ -11,7 +11,7 @@
 #include <bpf/libbpf.h>
 #include <bpf/bpf.h>
 #include "../lib/unity.h"
-
+#include "generate_packet.h"
 
 #define PACKET_SIZE 64
 #define BLACK_IP "192.192.192.192"
@@ -21,129 +21,83 @@
 #define DST_IP "127.0.0.1"
 #define SRC_PORT 80
 
+static int prog_fd;
+static char packet[PACKET_SIZE];
+static char malformed_packets[PACKET_SIZE * MALFORMED_CHECKS];
+static char fragmented_packet[PACKET_SIZE];
+static char pass_packet[PACKET_SIZE];
 
-struct test_result {
-    const char *test_name;
-    int expected_action;
-    int actual_action;
-    bool passed;
-};
-
-static void create_ipv4_packet(char *packet, __u8 protocol, const char *src_ip, const char *dst_ip, __u16 src_port, __u16 dst_port) {
-    struct ethhdr *eth = (struct ethhdr *)packet;
-    struct iphdr *ip = (struct iphdr *)(eth + 1);
-    
-    memset(packet, 0, PACKET_SIZE);
-    
-    eth->h_proto = htons(ETH_P_IP);
-    
-    ip->version = 4;
-    ip->ihl = 5;
-    ip->tot_len = htons(PACKET_SIZE - sizeof(struct ethhdr));
-    ip->protocol = protocol;
-    ip->saddr = inet_addr(src_ip);
-    ip->daddr = inet_addr(dst_ip);
-    
-    if (protocol == IPPROTO_TCP){
-        struct tcphdr *tcp = (struct tcphdr *)(ip + 1);
-        tcp->doff = 5;
-        tcp->source = htons(src_port);
-        tcp->dest = htons(dst_port);
-    }
-    else if (protocol == IPPROTO_UDP){
-        struct udphdr *udp = (struct udphdr *)(ip + 1);
-        udp->source = htons(src_port);
-        udp->dest = htons(dst_port);    
-    }
-}
-
-static struct test_result run_test_case(int prog_fd, const char *test_name, int expected_action, const char *packet) {
-    struct test_result result = {
-        .test_name = test_name,
-        .expected_action = expected_action,
-        .passed = false
-    };
-    
-    LIBBPF_OPTS(bpf_test_run_opts, opts,
-        .data_in = packet,
-        .data_size_in = PACKET_SIZE,
-        .repeat = 1,
-    );    
-    int err = bpf_prog_test_run_opts(prog_fd, &opts);
-    if (err) {
-        fprintf(stderr, "Failed to run test %s: %s\n",
-                test_name, strerror(errno));
-        return result;
-    }
-
-    result.actual_action = opts.retval;
-    result.passed = (result.actual_action == result.expected_action);
-
-    return result;
-}                
 
 void setUp(void) {
     // This function is called before each test case
-
+    
 }
 
 void tearDown(void) {
     // This function is called after each test case
 }
 
-void test_xdp_drop_black_ip(int prog_fd, const char* packet) {
-    LIBBPF_OPTS(bpf_test_run_opts, opts,
-        .data_in = packet,
-        .data_size_in = PACKET_SIZE,
-        .repeat = 1,
-    );    
-    int err = bpf_prog_test_run_opts(prog_fd, &opts);
-    TEST_ASSERT_EQUAL_INT(0, err);
-    TEST_ASSERT_EQUAL_INT(XDP_DROP, opts.retval);
+int run_opts_out(char * packet, int packet_size){
+    __u32 retval = 0;
+    int err = bpf_prog_test_run(prog_fd, 1, packet, packet_size,
+                                NULL, 0, &retval, NULL);
+    if (err){
+        fprintf(stderr, "Failed to run test\n");
+        return err;
+    }
+    return retval;
 }
 
-void test_xdp_drop_black_port(int prog_fd, const char* packet) {
-    LIBBPF_OPTS(bpf_test_run_opts, opts,
-        .data_in = packet,
-        .data_size_in = PACKET_SIZE,
-        .repeat = 1,
-    );    
-    int err = bpf_prog_test_run_opts(prog_fd, &opts);
-    TEST_ASSERT_EQUAL_INT(0, err);
-    TEST_ASSERT_EQUAL_INT(XDP_DROP, opts.retval);
+void test_expect_drop(void) {
+    create_ipv4_packet(packet, IPPROTO_TCP, BLACK_IP, DST_IP, SRC_PORT, ALLOWED_PORT);
+    TEST_ASSERT_EQUAL_INT(XDP_DROP, run_opts_out(packet, PACKET_SIZE));
+    
+    create_ipv4_packet(packet, IPPROTO_UDP, BLACK_IP, DST_IP, SRC_PORT, ALLOWED_PORT);
+    TEST_ASSERT_EQUAL_INT(XDP_DROP, run_opts_out(packet, PACKET_SIZE));
+    
+    create_ipv4_packet(packet, IPPROTO_TCP, ALLOWED_IP, DST_IP, SRC_PORT, BLACK_PORT);
+    TEST_ASSERT_EQUAL_INT(XDP_DROP, run_opts_out(packet, PACKET_SIZE));
+    
+    create_ipv4_packet(packet, IPPROTO_UDP, ALLOWED_IP, DST_IP, SRC_PORT, BLACK_PORT);
+    TEST_ASSERT_EQUAL_INT(XDP_DROP, run_opts_out(packet, PACKET_SIZE));
+    
+    create_non_tcp_udp_packet(packet, BLACK_IP, DST_IP);
+    TEST_ASSERT_EQUAL_INT(XDP_DROP, run_opts_out(packet, PACKET_SIZE));
+    
+    create_malformed_packets(malformed_packets);
+    for (int i = 0; i < MALFORMED_CHECKS; i++) {
+        int packet_size = PACKET_SIZE;
+        if (i == 0) {
+            packet_size = 10; // Packet too short for Ethernet header
+        } else if (i == 6) {
+            packet_size = 14 + 20 + 4; // Packet too short for UDP header
+        }
+        TEST_ASSERT_EQUAL_INT(XDP_DROP, run_opts_out(malformed_packets + PACKET_SIZE * i, packet_size));
+    }
+
+    create_fragmented_packet(fragmented_packet);
+    TEST_ASSERT_EQUAL_INT(XDP_DROP, run_opts_out(fragmented_packet, PACKET_SIZE));
 }
 
-void test_xdp_drop_malformed(int prog_fd, const char* packet) {
-    LIBBPF_OPTS(bpf_test_run_opts, opts,
-        .data_in = packet,
-        .data_size_in = PACKET_SIZE,
-        .repeat = 1,
-    );    
-    int err = bpf_prog_test_run_opts(prog_fd, &opts);
-    TEST_ASSERT_EQUAL_INT(0, err);
-    TEST_ASSERT_EQUAL_INT(XDP_DROP, opts.retval);
-}
+void test_expect_pass(void) {
+    // NOT blacklisted IP or port:
+    create_ipv4_packet(pass_packet, IPPROTO_TCP, ALLOWED_IP, DST_IP, SRC_PORT, ALLOWED_PORT);
+    TEST_ASSERT_EQUAL_INT(XDP_PASS, run_opts_out(pass_packet, PACKET_SIZE));
 
-void test_xdp_drop_fragments(int prog_fd, const char* packet) {
-    LIBBPF_OPTS(bpf_test_run_opts, opts,
-        .data_in = packet,
-        .data_size_in = PACKET_SIZE,
-        .repeat = 1,
-    );    
-    int err = bpf_prog_test_run_opts(prog_fd, &opts);
-    TEST_ASSERT_EQUAL_INT(0, err);
-    TEST_ASSERT_EQUAL_INT(XDP_DROP, opts.retval);
-}
+    create_ipv4_packet(pass_packet, IPPROTO_UDP, ALLOWED_IP, DST_IP, SRC_PORT, ALLOWED_PORT);
+    TEST_ASSERT_EQUAL_INT(XDP_PASS, run_opts_out(pass_packet, PACKET_SIZE));
 
-void test_xdp_pass(int prog_fd, const char* packet) {
-    LIBBPF_OPTS(bpf_test_run_opts, opts,
-        .data_in = packet,
-        .data_size_in = PACKET_SIZE,
-        .repeat = 1,
-    );    
-    int err = bpf_prog_test_run_opts(prog_fd, &opts);
-    TEST_ASSERT_EQUAL_INT(0, err);
-    TEST_ASSERT_EQUAL_INT(XDP_PASS, opts.retval);
+    // NOT TCP or UDP:
+    create_non_tcp_udp_packet(pass_packet, ALLOWED_IP, DST_IP);
+    TEST_ASSERT_EQUAL_INT(XDP_PASS, run_opts_out(pass_packet, PACKET_SIZE));
+
+    // Not IP:
+    create_non_ip_packet(pass_packet);
+    TEST_ASSERT_EQUAL_INT(XDP_PASS, run_opts_out(pass_packet, PACKET_SIZE));
+
+    // IPv6 packet:
+    create_ipv6_packet(pass_packet);
+    TEST_ASSERT_EQUAL_INT(XDP_PASS, run_opts_out(pass_packet, PACKET_SIZE));
 }
 
 
@@ -190,71 +144,21 @@ int main(int argc, char **argv) {
         bpf_object__close(obj);
         return 1;
     }
-    struct bpf_program *prog = bpf_object__find_program_by_name(obj, "xdp_filter");
+    struct bpf_program *prog = bpf_object__next_program(obj, NULL);
     if (!prog) {
         fprintf(stderr, "Failed to find XDP program\n");
         bpf_object__close(obj);
         return 1;
     }
     
-    int prog_fd = bpf_program__fd(prog);
+    prog_fd = bpf_program__fd(prog);
     
     UNITY_BEGIN();
     printf("=== Firewall XDP Filter Unit Tests ===\n\n");
 
-    // Define test cases
-    struct {
-        const char *name;
-        __u8 protocol;
-        const char *src_ip;
-        const char *dst_ip;
-        int src_port;
-        int dst_port;
-        int expected_action;
-    } test_cases[] = {
-        {"black ip (TCP) - packet should be dropped", IPPROTO_TCP,BLACK_IP, DST_IP, SRC_PORT, ALLOWED_PORT, XDP_DROP},
-        {"black port (TCP) - packet should be dropped", IPPROTO_TCP, ALLOWED_IP, DST_IP, SRC_PORT, BLACK_PORT, XDP_DROP},
-        {"black ip (UDP) - packet should be dropped", IPPROTO_UDP, BLACK_IP, DST_IP, SRC_PORT, ALLOWED_PORT, XDP_DROP},
-        {"black port (UDP) - packet should be dropped", IPPROTO_UDP, ALLOWED_IP, DST_IP, SRC_PORT, BLACK_PORT, XDP_DROP},
-        {"packet (TCP) - should pass", IPPROTO_TCP, ALLOWED_IP, DST_IP, SRC_PORT, ALLOWED_PORT, XDP_PASS},
-        {"packet (UDP) - should pass", IPPROTO_UDP, ALLOWED_IP, DST_IP, SRC_PORT, ALLOWED_PORT, XDP_PASS},
-    };
-
-    int passed_tests = 0;
-    int total_tests = sizeof(test_cases) / sizeof(test_cases[0]);
-
-    // Run all test cases
-    for (int i = 0; i < total_tests; i++) {
-        char packet[PACKET_SIZE];
-        create_ipv4_packet(packet, test_cases[i].protocol, test_cases[i].src_ip, test_cases[i].dst_ip,
-                                                           test_cases[i].src_port, test_cases[i].dst_port);
-        struct test_result result = run_test_case(
-            prog_fd,
-            test_cases[i].name,
-            test_cases[i].expected_action,
-            packet
-        );
-
-        printf("Test: %s\n", result.test_name);
-        printf("  Expected: %s (%d)\n",
-               result.expected_action == XDP_DROP ? "XDP_DROP" : "XDP_PASS",
-               result.expected_action);
-        printf("  Actual:   %s (%d)\n",
-               result.actual_action == XDP_DROP ? "XDP_DROP" : "XDP_PASS",
-               result.actual_action);
-        printf("  Result:   %s\n\n", result.passed ? "PASS" : "FAIL");
-
-        if (result.passed) {
-            passed_tests++;
-        }
-    }
-
-    printf("=== Test Summary ===\n");
-    printf("Passed: %d/%d tests\n", passed_tests, total_tests);
-    printf("Success rate: %.2f%%\n\n",
-           (double)passed_tests / total_tests * 100);
+    RUN_TEST(test_expect_drop);
+    RUN_TEST(test_expect_pass);
 
     bpf_object__close(obj);
     return UNITY_END();
-    // return (passed_tests == total_tests) ? 0 : 1;
 }
